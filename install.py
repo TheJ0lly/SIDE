0e7dd174a5c14e2b16cbaba925e7dd8d3332b5c9
TheJ0lly <eusicumine7@gmail.com>
2024-01-11 08:56:03 +0200

    Reduced the size of the binaries.

--- install.py
+++ install.py
@@ -27,17 +27,17 @@ if os_name == "Windows":
         location = sys.argv[1]
 
     print("Installing SIDE_Installer...")
-    err = os.system(f"go build -o {location}\\SIDE_Installer.exe installer.go")
+    err = os.system(f"go build -ldflags=\"-s -w\" -o {location}\\SIDE_Installer.exe installer.go")
 
     if err != 0:
         print("Go toolchain not installed on this machine! Consider installing the Go toolchain before installing!")
         sys.exit(2)
 
     print("Installing SIDE_Uninstaller...")
-    os.system(f"go build -o {location}\\SIDE_Uninstaller.exe uninstaller.go")
+    os.system(f"go build -ldflags=\"-s -w\" -o {location}\\SIDE_Uninstaller.exe uninstaller.go")
 
     print("Installing SIDE...")
-    os.system(f"go build -o {location}\\SIDE.exe main.go")
+    os.system(f"go build -ldflags=\"-s -w\" -o {location}\\SIDE.exe main.go")
 
 else:
     location = None
@@ -49,17 +49,17 @@ else:
         location = sys.argv[1]
 
     print("Installing SIDE_Installer...")
-    err = os.system(f"go build -o {location}/SIDE_Installer installer.go")
+    err = os.system(f"go build -ldflags=\"-s -w\" -o {location}/SIDE_Installer installer.go")
 
     if err != 0:
         print("Go toolchain not installed on this machine! Consider installing the Go toolchain before installing!")
         sys.exit(2)
 
     print("Installing SIDE_Uninstaller...")
-    os.system(f"go build -o {location}/SIDE_Uninstaller uninstaller.go")
+    os.system(f"go build -ldflags=\"-s -w\" -o {location}/SIDE_Uninstaller uninstaller.go")
 
     print("Installing SIDE...")
-    os.system(f"go build -o {location}/SIDE main.go")
+    os.system(f"go build -ldflags=\"-s -w\" -o {location}/SIDE main.go")
 
 
 print("\nSuccessfully installed SIDE.")
